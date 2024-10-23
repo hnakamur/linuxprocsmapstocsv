19c926eb9e816d2edb3e313d0547ae44a4ad501f
Hiroaki Nakamura <hnakamur@gmail.com>
2024-10-23 22:44:48 +0900

    Support format with variable field count

--- main.go
+++ main.go
@@ -6,22 +6,23 @@ import (
 	"encoding/csv"
 	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
-	"strings"
+	"reflect"
 	"unicode/utf8"
 )
 
 // https://docs.kernel.org/filesystems/proc.html
 
-type Args struct {
+type args struct {
 	inputFilename  string
 	outputFilename string
 	Separator      string
 }
 
-type Region struct {
+type region struct {
 	AddressStart []byte
 	AddressEnd   []byte
 	Perms        []byte
@@ -31,41 +32,18 @@ type Region struct {
 	Pathname     []byte
 }
 
-type Mapping struct {
-	Region          *Region
-	Size            string
-	KernelPageSize  string
-	MMUPageSize     string
-	Rss             string
-	Pss             string
-	Pss_Dirty       string
-	Shared_Clean    string
-	Shared_Dirty    string
-	Private_Clean   string
-	Private_Dirty   string
-	Referenced      string
-	Anonymous       string
-	KSM             string
-	LazyFree        string
-	AnonHugePages   string
-	ShmemPmdMapped  string
-	FilePmdMapped   string
-	Shared_Hugetlb  string
-	Private_Hugetlb string
-	Swap            string
-	SwapPss         string
-	Locked          string
-	THPeligible     string
-	ProtectionKey   string
-	VmFlags         string
+type mapping struct {
+	Region      *region
+	FieldNames  []string
+	FieldValues []string
 }
 
-var ErrBadFormat = errors.New("bad format")
+var errBadFormat = errors.New("bad format")
 
 const maxLineLength = 256
 
 func main() {
-	var args Args
+	var args args
 	flag.StringVar(&args.inputFilename, "i", "", "input filename to parse (in /proc/<pid>/smaps format)")
 	flag.StringVar(&args.outputFilename, "o", "", "output CSV filename")
 	flag.StringVar(&args.Separator, "sep", ",", "field separator")
@@ -84,7 +62,7 @@ func main() {
 	}
 }
 
-func run(args Args) error {
+func run(args args) error {
 	inputFile, err := os.Open(args.inputFilename)
 	if err != nil {
 		return err
@@ -100,112 +78,127 @@ func run(args Args) error {
 	w := csv.NewWriter(outputFile)
 	sep, _ := utf8.DecodeRuneInString(args.Separator)
 	w.Comma = sep
-	if err := w.Write([]string{
-		"AddressStart",
-		"AddressEnd",
-		"Perms",
-		"Offset",
-		"Dev",
-		"Inode",
-		"Pathname",
-		"Size",
-		"KernelPageSize",
-		"MMUPageSize",
-		"Rss",
-		"Pss",
-		"Pss_Dirty",
-		"Shared_Clean",
-		"Shared_Dirty",
-		"Private_Clean",
-		"Private_Dirty",
-		"Referenced",
-		"Anonymous",
-		"KSM",
-		"LazyFree",
-		"AnonHugePages",
-		"ShmemPmdMapped",
-		"FilePmdMapped",
-		"Shared_Hugetlb",
-		"Private_Hugetlb",
-		"Swap",
-		"SwapPss",
-		"Locked",
-		"THPeligible",
-		"ProtectionKey",
-		"VmFlags",
-	}); err != nil {
+	if err := convertSmapsToCsv(w, inputFile); err != nil {
 		return err
 	}
+	return err
+}
 
-	br := bufio.NewReaderSize(inputFile, maxLineLength)
+func convertSmapsToCsv(w *csv.Writer, r io.Reader) error {
+	br := bufio.NewReaderSize(r, maxLineLength)
+	var m mapping
+	var firstLineFieldLabels []string
+	regionIndex := -1
+	var prevRegionLineNo int
+	lineNo := 0
 	for {
-		// log.Printf("readling line %d", i)
-		m, err := readMapping(br)
+		line, err := readLine(br)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				break
 			}
 			return err
 		}
-		if err := w.Write(m.toCSVRecord()); err != nil {
-			return err
+		lineNo++
+
+		if isRegionLine(line) {
+			regionIndex++
+
+			if regionIndex > 0 {
+				if regionIndex == 1 {
+					if err := w.Write(m.toCSVHeader()); err != nil {
+						return err
+					}
+					firstLineFieldLabels = m.FieldNames
+				} else {
+					if err := m.checkFieldNames(firstLineFieldLabels, prevRegionLineNo); err != nil {
+						return err
+					}
+				}
+
+				if err := w.Write(m.toCSVRecord()); err != nil {
+					return err
+				}
+			}
+
+			r, err := parseRegion(line)
+			if err != nil {
+				return err
+			}
+			m.clear()
+			m.Region = r
+
+			prevRegionLineNo = lineNo
+		} else {
+			name, value, err := parseField(line)
+			if err != nil {
+				return err
+			}
+			m.appendField(string(name), string(value))
 		}
 	}
+
+	if err := m.checkFieldNames(firstLineFieldLabels, prevRegionLineNo); err != nil {
+		return err
+	}
+	if err := w.Write(m.toCSVRecord()); err != nil {
+		return err
+	}
 	w.Flush()
+
 	if err := w.Error(); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-const LF = '\n'
+const lf = '\n'
 
-func readMapping(r *bufio.Reader) (*Mapping, error) {
-	line, err := r.ReadBytes(LF)
+func readLine(r *bufio.Reader) ([]byte, error) {
+	line, err := r.ReadBytes(lf)
 	if err != nil {
 		return nil, err
 	}
-	// log.Printf("region line=%s", string(line))
-	region, err := parseRegion(line)
-	if err != nil {
-		return nil, err
-	}
-	m, err := readFields(r)
-	if err != nil {
-		return nil, err
-	}
-	m.Region = region
-	return m, nil
+	return bytes.TrimRight(line, "\n"), nil
 }
 
-func parseRegion(line []byte) (*Region, error) {
+func isRegionLine(line []byte) bool {
+	// Region line contains ASCII space before colon
+	// fcf0001000-fcf0002000 rw-p 00000000 00:00 0
+	i := bytes.IndexByte(line, ':')
+	if i == -1 {
+		panic("unexpected line format, no colon found")
+	}
+	return bytes.IndexByte(line[:i], ' ') != -1
+}
+
+func parseRegion(line []byte) (*region, error) {
 	addressStart, rest, ok := bytes.Cut(line, []byte{'-'})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	addressEnd, rest, ok := bytes.Cut(rest, []byte{' '})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	perms, rest, ok := bytes.Cut(rest, []byte{' '})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	offset, rest, ok := bytes.Cut(rest, []byte{' '})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	dev, rest, ok := bytes.Cut(rest, []byte{' '})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	inode, rest, ok := bytes.Cut(rest, []byte{' '})
 	if !ok {
-		return nil, ErrBadFormat
+		return nil, errBadFormat
 	}
 	pathname := bytes.TrimSpace(rest)
-	return &Region{
+	return &region{
 		AddressStart: addressStart,
 		AddressEnd:   addressEnd,
 		Perms:        perms,
@@ -216,88 +209,31 @@ func parseRegion(line []byte) (*Region, error) {
 	}, nil
 }
 
-func readFields(r *bufio.Reader) (*Mapping, error) {
-	var m Mapping
-	if err := readField(r, "Size", &m.Size); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "KernelPageSize", &m.KernelPageSize); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "MMUPageSize", &m.MMUPageSize); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Rss", &m.Rss); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Pss", &m.Pss); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Pss_Dirty", &m.Pss_Dirty); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Shared_Clean", &m.Shared_Clean); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Shared_Dirty", &m.Shared_Dirty); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Private_Clean", &m.Private_Clean); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Private_Dirty", &m.Private_Dirty); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Referenced", &m.Referenced); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Anonymous", &m.Anonymous); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "KSM", &m.KSM); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "LazyFree", &m.LazyFree); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "AnonHugePages", &m.AnonHugePages); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "ShmemPmdMapped", &m.ShmemPmdMapped); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "FilePmdMapped", &m.FilePmdMapped); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Shared_Hugetlb", &m.Shared_Hugetlb); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Private_Hugetlb", &m.Private_Hugetlb); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Swap", &m.Swap); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "SwapPss", &m.SwapPss); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "Locked", &m.Locked); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "THPeligible", &m.THPeligible); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "ProtectionKey", &m.ProtectionKey); err != nil {
-		return nil, err
-	}
-	if err := readField(r, "VmFlags", &m.VmFlags); err != nil {
-		return nil, err
-	}
-	return &m, nil
+func (m *mapping) clear() {
+	m.Region = nil
+	m.FieldNames = nil
+	m.FieldValues = nil
 }
 
-func (m *Mapping) toCSVRecord() []string {
-	return []string{
+func (m *mapping) appendField(name, value string) {
+	m.FieldNames = append(m.FieldNames, name)
+	m.FieldValues = append(m.FieldValues, value)
+}
+
+func (m *mapping) toCSVHeader() []string {
+	return append([]string{
+		"AddressStart",
+		"AddressEnd",
+		"Perms",
+		"Offset",
+		"Dev",
+		"Inode",
+		"Pathname",
+	}, m.FieldNames...)
+}
+
+func (m *mapping) toCSVRecord() []string {
+	return append([]string{
 		string(m.Region.AddressStart),
 		string(m.Region.AddressEnd),
 		string(m.Region.Perms),
@@ -305,105 +241,29 @@ func (m *Mapping) toCSVRecord() []string {
 		string(m.Region.Dev),
 		string(m.Region.Inode),
 		string(m.Region.Pathname),
-		m.Size,
-		m.KernelPageSize,
-		m.MMUPageSize,
-		m.Rss,
-		m.Pss,
-		m.Pss_Dirty,
-		m.Shared_Clean,
-		m.Shared_Dirty,
-		m.Private_Clean,
-		m.Private_Dirty,
-		m.Referenced,
-		m.Anonymous,
-		m.KSM,
-		m.LazyFree,
-		m.AnonHugePages,
-		m.ShmemPmdMapped,
-		m.FilePmdMapped,
-		m.Shared_Hugetlb,
-		m.Private_Hugetlb,
-		m.Swap,
-		m.SwapPss,
-		m.Locked,
-		m.THPeligible,
-		m.ProtectionKey,
-		m.VmFlags,
-	}
+	}, m.FieldValues...)
 }
 
-func readField(r *bufio.Reader, name string, field *string) error {
-	line, err := r.ReadBytes(LF)
-	if err != nil {
-		return err
-	}
-	line = bytes.TrimRight(line, string(LF))
-	gotName, rest, ok := bytes.Cut(line, []byte{':'})
-	if !ok || string(gotName) != name {
-		return ErrBadFormat
-	}
-
-	rest = bytes.TrimLeft(rest, " ")
-	var value []byte
-	switch name {
-	case "THPeligible", "ProtectionKey", "VmFlags":
-		value = rest
-	default:
-		value, rest, ok = bytes.Cut(bytes.TrimLeft(rest, " "), []byte{' '})
-		if !ok || !bytes.Equal(rest, []byte("kB")) {
-			return ErrBadFormat
-		}
-	}
-	*field = string(value)
-	return nil
-}
-
-func (r *Region) writeCsv(w io.Writer) error {
-	if _, err := w.Write(r.AddressStart); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.AddressEnd); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.Perms); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.Offset); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.Dev); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.Inode); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{','}); err != nil {
-		return err
-	}
-	if _, err := w.Write(r.Pathname); err != nil {
-		return err
+func (m *mapping) checkFieldNames(firstLineFieldNames []string, regionLineNo int) error {
+	if !reflect.DeepEqual(m.FieldNames, firstLineFieldNames) {
+		return fmt.Errorf("field names mismatch betweeen the first region and the region at line %d\n"+
+			"fields in first region:%v\n"+
+			"feilds in region at line %d:%v",
+			regionLineNo, firstLineFieldNames,
+			regionLineNo, m.FieldNames)
 	}
 	return nil
 }
 
-func (r *Region) String() string {
-	var b strings.Builder
-	r.writeCsv(&b)
-	return b.String()
+func parseField(line []byte) (name, value []byte, err error) {
+	name, rest, ok := bytes.Cut(line, []byte{':'})
+	if !ok {
+		return nil, nil, errBadFormat
+	}
+
+	value = bytes.TrimLeft(rest, " ")
+	if !bytes.Equal(name, []byte("VmFlags")) {
+		value, _, _ = bytes.Cut(value, []byte{' '})
+	}
+	return name, value, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,17 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestParseRegion(t *testing.T) {
 	r, err := parseRegion([]byte("4d400283000-4d400284000 ---p 00000000 00:00 0                            [anon:partition_alloc]"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	if got, want := r.String(), "4d400283000,4d400284000,---p,00000000,00:00,0,[anon:partition_alloc]"; got != want {
+	m := mapping{Region: r}
+	if got, want := strings.Join(m.toCSVRecord(), ","), "4d400283000,4d400284000,---p,00000000,00:00,0,[anon:partition_alloc]"; got != want {
 		t.Errorf("result mismatch,\n got=%s,\nwant=%s", got, want)
 	}
 }
